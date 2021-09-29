a55d20eae3516b59a497d3cdb00661e5625d936d
Sim_jae_ho <tlawogh45@naver.com>
2021-09-30 00:30:44 +0900

    Update UI/UX & Add Customer Search Module

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createTheme} from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: '"Noto Sans KR", serif',
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}><App /></ThemeProvider>
+</React.StrictMode>,
   document.getElementById('root')
 );
 
